2cfa6d7b2b9069f39eae0d491f1af6fbcd739062
Parv Kothari <parvkothari11@gmail.com>
2023-05-30 20:19:33 +0530

    feat: initial-commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1 className="text-3xl text-red-400 font-bold underline">
+          Hello world!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
